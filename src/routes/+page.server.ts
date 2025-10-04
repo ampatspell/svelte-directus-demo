f434fa549cec506f6105113fd291051e48525f36
Arnis Vuskans <ampatspell@gmail.com>
2025-10-05 01:57:55 +0300

    random

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,6 +6,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ fetch }) => {
   const directus = getDirectus(fetch);
   return {
-    hello: await directus.request(readSingleton(CollectionNames.hello, {}))
+    hello: await directus.request(readSingleton(CollectionNames.hello)),
+    install: await directus.request(readSingleton(CollectionNames.install)),
   };
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,19 @@
   let hello = $derived(data.hello);
 </script>
 
-<div class="hello">{hello.message}</div>
+<div class="hello">
+  <div class="message">{hello.message}</div>
+  <div class="install">{data.install.content}</div>
+</div>
 
 <style lang="scss">
   .hello {
     padding: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 100px;
+    > .install {
+      white-space: pre-wrap;
+    }
   }
 </style>
